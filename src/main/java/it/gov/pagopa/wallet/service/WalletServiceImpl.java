2d83798b5ec20a5757db57bbad68ab67577e2ad9
larissaaiala <larissaaialasilva@gmail.com>
2022-08-10 15:22:18 +0200

    [ISB-312] Correzione consumer iban

--- src/main/java/it/gov/pagopa/wallet/service/WalletServiceImpl.java
+++ src/main/java/it/gov/pagopa/wallet/service/WalletServiceImpl.java
@@ -176,8 +176,6 @@ public class WalletServiceImpl implements WalletService {
         .build();
     timelineProducer.sendEvent(queueOperationDTO);
 
-
-    this.deleteOperation(new IbanQueueWalletDTO());
   }
 
   @Override
