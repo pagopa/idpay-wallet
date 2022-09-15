95ead684882a7e0809592b76a2a9bc1bea1a31df
Marco Di Lisa <marco.di.lisa@nttdata.com>
2022-09-15 13:58:39 +0200

    Removed modelMapper

--- src/main/java/it/gov/pagopa/wallet/dto/mapper/WalletMapper.java
+++ src/main/java/it/gov/pagopa/wallet/dto/mapper/WalletMapper.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.wallet.dto.mapper;
 
 import it.gov.pagopa.wallet.dto.EvaluationDTO;
+import it.gov.pagopa.wallet.dto.InitiativeDTO;
 import it.gov.pagopa.wallet.enums.WalletStatus;
 import it.gov.pagopa.wallet.model.Wallet;
 import java.math.BigDecimal;
@@ -22,6 +23,20 @@ public class WalletMapper {
         .amount(evaluationDTO.getBeneficiaryBudget())
         .accrued(BigDecimal.valueOf(0.00))
         .refunded(BigDecimal.valueOf(0.00))
+        .build();
+  }
+
+  public InitiativeDTO toInitiativeDTO(Wallet wallet){
+    return InitiativeDTO.builder()
+        .initiativeId(wallet.getInitiativeId())
+        .initiativeName(wallet.getInitiativeName())
+        .endDate(wallet.getEndDate())
+        .status(wallet.getStatus())
+        .amount(wallet.getAmount())
+        .accrued(wallet.getAccrued())
+        .refunded(wallet.getRefunded())
+        .nInstr(String.valueOf(wallet.getNInstr()))
+        .iban(wallet.getIban())
         .build();
   }
 }
--- src/main/java/it/gov/pagopa/wallet/service/WalletServiceImpl.java
+++ src/main/java/it/gov/pagopa/wallet/service/WalletServiceImpl.java
@@ -31,13 +31,11 @@ import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.iban4j.CountryCode;
 import org.iban4j.Iban;
 import org.iban4j.IbanUtil;
 import org.iban4j.UnsupportedCountryException;
-import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -78,13 +76,12 @@ public class WalletServiceImpl implements WalletService {
 
   @Override
   public InitiativeDTO getWalletDetail(String initiativeId, String userId) {
-    Optional<Wallet> wallet = walletRepository.findByInitiativeIdAndUserId(initiativeId, userId);
-    return wallet
-        .map(this::walletToDto)
-        .orElseThrow(
-            () ->
-                new WalletException(
-                    HttpStatus.NOT_FOUND.value(), WalletConstants.ERROR_WALLET_NOT_FOUND));
+    Wallet wallet = walletRepository.findByInitiativeIdAndUserId(initiativeId, userId).orElseThrow(
+        () ->
+            new WalletException(
+                HttpStatus.NOT_FOUND.value(), WalletConstants.ERROR_WALLET_NOT_FOUND));
+    return walletMapper.toInitiativeDTO(wallet);
+
   }
 
   @Override
@@ -187,7 +184,7 @@ public class WalletServiceImpl implements WalletService {
     List<InitiativeDTO> initiativeDTOList = new ArrayList<>();
 
     for (Wallet wallet : walletList) {
-      initiativeDTOList.add(walletToDto(wallet));
+      initiativeDTOList.add(walletMapper.toInitiativeDTO(wallet));
     }
     initiativeListDTO.setInitiativeList(initiativeDTOList);
     return initiativeListDTO;
@@ -318,11 +315,6 @@ public class WalletServiceImpl implements WalletService {
         .status(WalletConstants.STATUS_KO)
         .build();
     notificationProducer.sendCheckIban(notificationQueueDTO);
-  }
-
-  private InitiativeDTO walletToDto(Wallet wallet) {
-    ModelMapper modelmapper = new ModelMapper();
-    return modelmapper.map(wallet, InitiativeDTO.class);
   }
 
   private void formalControl(String iban) {
