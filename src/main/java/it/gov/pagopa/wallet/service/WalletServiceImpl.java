2c417f543310c5f21edafde35e2ae8345375e6fb
Marco Di Lisa <marco.di.lisa@nttdata.com>
2022-11-16 17:29:05 +0100

    [ISB-596] Wallet update from refund

--- src/main/java/it/gov/pagopa/wallet/service/WalletServiceImpl.java
+++ src/main/java/it/gov/pagopa/wallet/service/WalletServiceImpl.java
@@ -286,6 +286,7 @@ public class WalletServiceImpl implements WalletService {
 
   @Override
   public void processRefund(RefundDTO refundDTO) {
+    log.info("[PROCESS_REFUND] Processing new refund: {}", refundDTO);
 
     Wallet wallet =
         walletRepository
