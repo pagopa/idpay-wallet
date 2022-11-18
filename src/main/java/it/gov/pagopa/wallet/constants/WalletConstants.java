29d19d273fc6e45966649437e0ba103f527181c0
Marco Di Lisa <marco.di.lisa@nttdata.com>
2022-11-18 12:04:27 +0100

    [ISB-598] Added fields to NotificationQueueDTO + produced notification for refund

--- src/main/java/it/gov/pagopa/wallet/constants/WalletConstants.java
+++ src/main/java/it/gov/pagopa/wallet/constants/WalletConstants.java
@@ -23,7 +23,7 @@ public class WalletConstants {
   public static final String ERROR_MSG_HEADER_MESSAGE = "rootCauseMessage";
   public static final String KAFKA= "kafka";
 
-  public static final String ERROR_TIMELINE= "Error to sending event to timeline queue";
+  public static final String ERROR_QUEUE= "Error to sending event to queue";
   public static final String CHANNEL_PM= "PAYMENT-MANAGER";
   public static final String ERROR_LESS_THAN_ZERO = "The field must be greater than zero!";
 
--- src/main/java/it/gov/pagopa/wallet/dto/NotificationQueueDTO.java
+++ src/main/java/it/gov/pagopa/wallet/dto/NotificationQueueDTO.java
@@ -1,10 +1,14 @@
 package it.gov.pagopa.wallet.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonInclude(Include.NON_NULL)
 public class NotificationQueueDTO {
   private String operationType;
   private String userId;
@@ -12,5 +16,11 @@ public class NotificationQueueDTO {
   private String serviceId;
   private String iban;
   private String status;
-
+  private String rewardNotificationId;
+  private String refundCro;
+  private LocalDateTime refundDate;
+  private Long refundReward;
+  private String rejectionCode;
+  private String rejectionReason;
+  private Long refundFeedbackProgressive;
 }
