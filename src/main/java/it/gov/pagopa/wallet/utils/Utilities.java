60d0ef89b3159c5658b8706a6b6ccfe3b4143442
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-04-30 16:58:09 +0200

    github workflow update

--- src/main/java/it/gov/pagopa/wallet/utils/Utilities.java
+++ src/main/java/it/gov/pagopa/wallet/utils/Utilities.java
@@ -22,12 +22,14 @@ public class Utilities {
 
     public String exceptionConverter(FeignException e) {
         String error;
+
         try {
             ErrorDTO errorDTO = objectMapper.readValue(e.contentUTF8(), ErrorDTO.class);
             error = errorDTO.getMessage() != null ? errorDTO.getMessage() : e.getMessage();
         } catch (Exception ex) {
             error= e.getMessage();
         }
+
         return error;
     }
 
