15dcabb687b8a9c2a5a76d266837064b474ce422
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2026-02-19 11:39:09 +0100

    fix UtilitiesTest

--- src/test/java/it/gov/pagopa/wallet/utils/UtilitiesTest.java
+++ src/test/java/it/gov/pagopa/wallet/utils/UtilitiesTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashMap;
@@ -30,7 +30,7 @@ class UtilitiesTest {
     @Autowired
     Utilities utilities;
 
-    @MockBean
+    @MockitoBean
     ObjectMapper objectMapper;
 
     private static final String BAD_REQUEST = "BAD REQUEST";
