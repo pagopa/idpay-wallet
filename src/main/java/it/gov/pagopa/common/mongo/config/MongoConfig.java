00cc5430a4473179576b3e81ac3b1899e4aadea2
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2026-02-27 13:26:47 +0100

    test mongo

--- src/main/java/it/gov/pagopa/common/mongo/config/MongoConfig.java
+++ src/main/java/it/gov/pagopa/common/mongo/config/MongoConfig.java
@@ -26,7 +26,7 @@ import java.util.concurrent.TimeUnit;
 public class MongoConfig {
         
     @Configuration
-    @ConfigurationProperties(prefix = "spring.data.mongodb.config")
+    @ConfigurationProperties(prefix = "spring.mongodb.config")
     public static class MongoDbCustomProperties {
         @Setter
         ConnectionPoolSettings connectionPool;
