f3f50e65ac7eb23fb50bfe84049c71ea6373d520
and-mora <andrea.morabito@pagopa.it>
2026-03-10 17:16:31 +0100

    feat: introduce RewardCalculatorInvocationException and update RewardCalculatorRestConnectorImpl for error handling

--- src/main/java/it/gov/pagopa/wallet/connector/RewardCalculatorRestConnectorImpl.java
+++ src/main/java/it/gov/pagopa/wallet/connector/RewardCalculatorRestConnectorImpl.java
@@ -1,11 +1,11 @@
 package it.gov.pagopa.wallet.connector;
 
+import static it.gov.pagopa.wallet.constants.WalletConstants.ExceptionMessage.ERROR_REWARD_CALCULATOR_INVOCATION_MSG;
+
 import feign.FeignException;
-import it.gov.pagopa.wallet.exception.custom.PaymentInvocationException;
+import it.gov.pagopa.wallet.exception.custom.RewardCalculatorInvocationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import static it.gov.pagopa.wallet.constants.WalletConstants.ExceptionMessage.ERROR_PAYMENT_INVOCATION_MSG;
 
 @Service
 @Slf4j
@@ -22,9 +22,12 @@ public class RewardCalculatorRestConnectorImpl implements RewardCalculatorRestCo
     try {
       rewardCalculatorRestClient.createOnboardingCounters(initiativeId, userId);
     } catch (FeignException e) {
-      log.error("[CREATE_ONBOARDING_COUNTERS] An error occurred while invoking reward calculator for initiative {} and user {}",
-          initiativeId, userId);
-      throw new PaymentInvocationException(ERROR_PAYMENT_INVOCATION_MSG, true, e);
+      log.error(
+          "[CREATE_ONBOARDING_COUNTERS] An error occurred while invoking reward calculator for initiative {} and user {}",
+          initiativeId,
+          userId);
+      throw new RewardCalculatorInvocationException(
+          ERROR_REWARD_CALCULATOR_INVOCATION_MSG, true, e);
     }
   }
 }
