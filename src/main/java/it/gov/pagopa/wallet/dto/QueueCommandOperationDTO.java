a58af1d213738beed5253df9c2c43abd924ce0fa
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@emeal.nttdata.com>
2026-07-16 17:50:24 +0200

    command dto arg costruct

--- src/main/java/it/gov/pagopa/wallet/dto/QueueCommandOperationDTO.java
+++ src/main/java/it/gov/pagopa/wallet/dto/QueueCommandOperationDTO.java
@@ -1,11 +1,13 @@
 package it.gov.pagopa.wallet.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class QueueCommandOperationDTO {
 
